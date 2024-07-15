0e0b9a15b140d77169bd22aeb1fd053e921a3b88
Shreyank Agrawal <122221017+Shreyank108@users.noreply.github.com>
2024-07-15 11:59:48 +0530

    Update settings.py

--- salvador/settings.py
+++ salvador/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path 
 import os 
 
-STATIC_URL = '/static/'
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2l)4ghkjdtrq-d6gq^_ch_&z7-ccz(&&70lvl70g42^@+rrjzo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -127,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
+STATIC_URL = STATIC_HOST + "/static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
